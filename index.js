31d96eed56867cbfb94ff6410e089b0e1cbeba0c
YuriTropeia <yurineudinetropeia@gmail.com>
2024-08-23 13:30:09 -0300

    implementação POST

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import http from "http";
+import {v4} from 'uuid';
 
 const port = 3000
 const grades = [
@@ -11,15 +12,33 @@ const grades = [
 
 const server = http.createServer((request, response)=>{
     // funções do backend
-    const {method, url} = request
+    const {method, url} = request;
+    let body = '';
 
-    if (url === '/grades' && method === 'GET'){
-        response.writeHead(200, {'Content-Type': 'application/json'});
-        response.end(JSON.stringify(grades));
-    }else{
-        response.writeHead(404, {'Content-Type': 'application/json'});
-        response.end(JSON.stringify({message: 'Route not found'}));
-    }
+    request.on('data', (chunk) =>{
+        body += chunk.toString();
+    });
+
+    request.on('end', () =>{
+        
+        if (url === '/grades' && method === 'GET'){
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify(grades));
+        }
+        
+        else if(url === '/grades' && method === 'POST'){
+            const {studentName, subject, grade} = JSON.parse(body);
+            const newGrade = {id: v4(), studentName, subject, grade};
+            grades.push(newGrade);
+            response.writeHead(201, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify(newGrade));
+        }
+        
+        else{
+            response.writeHead(404, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify({message: 'Route not found'}));
+        }
+    });
 });
 
 server.listen(port, () => {
