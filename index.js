ebfe518848031b46b7781370d47dd9ed6f7820f3
YuriTropeia <yurineudinetropeia@gmail.com>
2024-08-23 14:14:56 -0300

    implementação PUT

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const server = http.createServer((request, response)=>{
     });
 
     request.on('end', () =>{
+        const id = url.split('/')[2];
         
         if (url === '/grades' && method === 'GET'){
             response.writeHead(200, {'Content-Type': 'application/json'});
@@ -32,6 +33,21 @@ const server = http.createServer((request, response)=>{
             grades.push(newGrade);
             response.writeHead(201, {'Content-Type': 'application/json'});
             response.end(JSON.stringify(newGrade));
+        }
+
+        else if(url.startsWith('/grades/') && method === 'PUT'){
+            const {studentName, subject, grade} = JSON.parse(body);
+            const gradeToUpdate = grades.find( g => g.id === id);
+            if (gradeToUpdate){
+                gradeToUpdate.studentName = studentName;
+                gradeToUpdate.subject = subject;
+                gradeToUpdate.grade = grade;
+                response.writeHead(200, {'Content-Type': 'application/json'});
+                response.end(JSON.stringify(gradeToUpdate));
+            } else{
+                response.writeHead(404, {'Content-Type': 'application/json'});
+                response.end(JSON.stringify({message: 'Grade not found'}));
+            }
         }
         
         else{
